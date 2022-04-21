5286fa4bc637aa2abeec470bb4d3252018959899
shivamyneni <shivamyneni4816@gmail.com>
2022-04-21 14:13:07 +0530

    main

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./containers/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      xs: { max: "640px" },
+
+      ...defaultTheme.screens,
+    },
+    extend: {
+      fontFamily: {
+        alphaSlabOne: ["Alfa Slab One"],
+        Inter: ["Inter"],
+        SquarePeg: ["Square Peg"],
+      },
+    },
+  },
+  plugins: [],
+};
