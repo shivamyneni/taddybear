f22e9d61bd4516e6f1013d3b6409fe4d3e73c4b6
shivamyneni <shivamyneni4816@gmail.com>
2022-04-25 13:31:12 +0530

    design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,26 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
 import styles from "../styles/Home.module.css";
 import { gsap } from "gsap";
 import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
 import { DiGoogleDrive } from "react-icons/di";
-import { useEffect } from "react";
+import { RiBearSmileLine } from "react-icons/ri";
+import { useEffect, useState } from "react";
 const Home: NextPage = () => {
+  gsap.registerPlugin(ScrollToPlugin);
   useEffect(() => {
     gsap
       .timeline()
       .to(".heading", { duration: 1, opacity: 1 })
       .to(".subheading", { duration: 1, opacity: 1 });
   }, []);
+
+  const [scrollId, setScrollId] = useState("header");
+  useEffect(() => {
+    gsap.to(window, { duration: 1, scrollTo: `.${scrollId}` });
+  }, [scrollId]);
   return (
     <div className="w-screen h-auto">
       <Head>
@@ -20,13 +28,44 @@ const Home: NextPage = () => {
         <meta name="description" content="Fundraising Initiative" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <div className="fixed xs:px-[40px] sm:px-[40px] md:px-[90px] xs:py-[20px] sm:py-[20px] md:py-[50px]">
-        <RiBearSmileLine
-          color="white"
-          size={60}
-          className="bg-[#c49a11ba] rounded-full p-[15px] "
-        />
-      </div> */}
+      <div className="fixed xs:px-[30px] sm:px-[30px] md:px-[50px] xs:py-[20px] sm:py-[20px] md:py-[10px] bg-white w-screen shadow-md flex flex-row items-center justify-between header">
+        <div className="flex flex-row items-center self-start">
+          <RiBearSmileLine
+            color="white"
+            size={50}
+            className="bg-[#ffd727] rounded-full p-[10px] "
+          />
+          <p className="text-[#ffd727] font-SquarePeg font-bold  xs:text-[20px] sm:text-[20px] md:text-[25px] ml-[10px] md:flex sm:hidden xs:hidden">
+            ThaddyBear
+          </p>
+        </div>
+        <div className="w-1/2 h-full flex flex-row items-center justify-end">
+          <p
+            className="text-[#ffd727] font-Inter font-bold  xs:text-[20px] sm:text-[20px] md:text-[15px] ml-[20px] md:flex sm:hidden xs:hidden"
+            onClick={() => setScrollId("about")}
+          >
+            About
+          </p>
+          <p
+            className="text-[#ffd727] font-Inter font-bold  xs:text-[20px] sm:text-[20px] md:text-[15px] ml-[20px] md:flex sm:hidden xs:hidden"
+            onClick={() => setScrollId("journey")}
+          >
+            Journey
+          </p>
+          <p
+            className="text-[#ffd727] font-Inter font-bold  xs:text-[20px] sm:text-[20px] md:text-[15px] ml-[20px] md:flex sm:hidden xs:hidden"
+            onClick={() => setScrollId("updates")}
+          >
+            Updates
+          </p>
+          <p
+            className="text-[#ffd727] font-Inter font-bold  xs:text-[20px] sm:text-[20px] md:text-[15px] ml-[20px] md:flex sm:hidden xs:hidden"
+            onClick={() => setScrollId("contactus")}
+          >
+            Contact Us
+          </p>
+        </div>
+      </div>
       <div className="w-screen h-screen flex flex-col items-center justify-center  Imagebackground xs:px-[40px] sm:px-[40px] md:px-[90px] ">
         <p className="text-white font-Inter font-bold xs:text-[30px] sm:text-[30px] md:text-[50px] capitalize heading opacity-0">
           {`"no act of kindness, no matter how small is ever wasted."`}
@@ -35,43 +74,46 @@ const Home: NextPage = () => {
           {`Help us bring smile on taddy's face.`}
         </p> */}
       </div>
-      <div className="w-screen xs:h-auto sm:h-auto md:h-screen flex xs:flex-col sm:flex-col md:flex-row flex-wrap overflow-hidden">
+      <div className="w-screen xs:h-auto sm:h-auto md:h-auto flex xs:flex-col sm:flex-col md:flex-row flex-wrap overflow-hidden about">
         <div className="xs:w-full sm:w-full md:w-1/2 aspect-square bg-white flex flex-col items-center justify-center px-[40px]">
           <p className="text-black font-Inter font-bold xs:text-[25px] sm:text-[25px] md:text-[40px] self-start mt-[20px] mb-[20px] capitalize">
             {`"About Thaddy"`}
           </p>
-          <p className="text-black font-Inter font-normal text-[15px] capitalize">
-            Thaddeus Luke Thomas was diagnosed with High Risk Neuroblastoma at 7
-            years of age in Oct 21. Before his diagnosis, Thaddeus was an active
-            boy who loved school, nature, travelling as well as family day-out
-            shopping and dining. He also enjoyed swimming and had a keen
-            interest in Taekwondo and keyboard . Thaddeus lists playing in the
-            snow and eating ramen as his all-time favourite activities.
+          <p className="text-black font-Inter font-normal text-[15px] ">
+            Thaddeus was diagnosed with High Risk Neuroblastoma at 7 years of
+            age in Oct 2021. He is an active boy who loved school, nature,
+            travelling as well as family day-out shopping and dining. He also
+            enjoys swimming and has a keen interest in Taekwondo and playing the
+            keyboard . Although he cannot do most of these activities now,
+            Thaddeus is staying positive and indulging in other hobbies such as
+            drawing and organising.
           </p>
           <br />
           <br />
 
-          <p className="text-black font-Inter font-normal text-[15px] capitalize">
-            In Oct 21, Thaddeus had a prolonged bout of fever and complained of
-            pain in his legs and buttocks. Abnormal X-rays led to more
-            investigations that finally revealed that the Neuroblastoma had
-            spread to his bone marrow, chest, shoulders, legs and skull. The
-            tumour also grown out from his chest and compressed his diaphragm
-            and adrenal gland.
+          <p className="text-black font-Inter font-normal text-[15px] ">
+            Thaddeus started having persistent fever episodes from Sep 2021 to
+            Oct 2021. His visit to Accident & Emergency in Sep 2021 at Sengkang
+            General Hospital did not diagnose his cancer. In Oct 2021, he
+            started to have difficulty walking and complained of severe pain in
+            his chest and lower body. He was brought to KKH Accident & Emergency
+            and an X-ray showed some abnormalities. Further tests revealed that
+            he had a tumor in his abdomen that traversed to the chest and that
+            it had spread to his bone marrow and bones.
           </p>
           <br />
 
-          <p className="text-black font-Inter font-normal text-[15px] capitalize">
+          <p className="text-black font-Inter font-normal text-[15px] ">
             Thaddeus’s life changed overnight and he had to get used to invasive
             procedures and long stays in the hospital. It was a tough time for
             him and his family as they adapted to this new lifestyle. With the
-            treatment, Thaddeus’s immune system is very low and he is not able
+            treatment, Thaddeus’s immune system is very weak and he is not able
             to pursue his hobbies and indulge in most of his favourite
             activities. However, Thaddeus has amazed his family with his
             positivity and toughness, undergoing 2-hour long MRIs without
-            general anasthesia, enduring a 13-hour long surgery and countless
-            procedures involving needles and tubes in his chest and down his
-            throat.
+            general anasthesia, enduring a 13-hour long surgery, bone marrow
+            aspirations, blood tests, multiple scans and countless procedures
+            involving needles and tubes in his chest and down his throat.
           </p>
         </div>
         <div className="xs:w-full sm:w-full md:w-1/2 flex flex-wrap flex-row bg-white justify-end ">
@@ -80,11 +122,7 @@ const Home: NextPage = () => {
             src="/Images/Active5.png"
             className="xs:w-full sm:w-full md:w-auto md:mt-[0px] xs:mt-[40px] sm:mt-[40px] "
           />
-          {/* <img
-            alt=""
-            src="/Images/Active2.png"
-            className="xs:w-full sm:w-full md:w-auto "
-          /> */}
+
           <img
             alt=""
             src="/Images/Active3.png"
@@ -101,19 +139,31 @@ const Home: NextPage = () => {
         <p className="text-black font-Inter font-bold xs:text-[25px] sm:text-[25px] md:text-[40px] self-start mb-[20px] capitalize">
           {`"What is Neuroblastoma?"`}
         </p>
-        <p className="text-black font-Inter font-normal text-[15px] capitalize w-4/5">
+        <p className="text-black font-Inter font-normal text-[15px]  w-4/5">
           Neuroblastoma is a rare form of childhood cancer that affects less
           than 10 children in Singapore each year. It develops in the nerve
-          tissue and can spread to other parts of the body including the lymph
-          nodes, bones, bone marrow and soft tissue. High risk neuroblastoma is
-          very difficult to treat and requires multi modal treatment that last
-          anywhere between 1 to 3 years. Children with Neuroblastoma have to go
-          through chemotherapy, surgery, radiation, stem cell transplant,
-          immunotherapy and maintenance treatment. There is also a high
-          probability of relapse, which will require even more treatment.
+          cells and can spread to other parts of the body including the lymph
+          nodes, bones, bone marrow and soft tissue. High risk Neuroblastoma is
+          very difficult to treat and requires multimodal treatment that usually
+          lasts more than a year. Children with High Risk Neuroblastoma have to
+          go through chemotherapy, surgery, stem cell transplant, radiation
+          therapy, immunotherapy and maintenance treatment.
+        </p>
+        <br />
+
+        <p className="text-black font-Inter font-normal text-[15px]  w-4/5">
+          Neuroblastoma develops in the nerve tissue and can spread to other
+          parts of the body including the lymph nodes, bones, bone marrow and
+          soft tissue. High risk neuroblastoma is very difficult to treat and
+          requires multi modal treatments that last more than 18 months.
+          Children with Neuroblastoma have to go through chemotherapy, surgery,
+          radiation, stem cell transplant, immunotherapy and maintenance
+          treatment. The side effects are extensive and rough on the children.
+          In addition, children undergoing treatment are immune compromised and
+          fall sick very easily.
         </p>
       </div>
-      <div className="w-screen xs:h-auto sm:h-auto md:h-screen flex xs:flex-col sm:flex-col md:flex-row flex-wrap overflow-hidden">
+      <div className="w-screen xs:h-auto sm:h-auto md:h-auto flex xs:flex-col sm:flex-col md:flex-row flex-wrap overflow-hidden journey">
         <div className="xs:w-full sm:w-full md:w-1/2 flex flex-wrap flex-row bg-white ">
           <img
             alt=""
@@ -140,7 +190,7 @@ const Home: NextPage = () => {
           <p className="text-black font-Inter font-bold xs:text-[25px] sm:text-[25px] md:text-[40px]  self-start  mt-[20px] mb-[20px] capitalize">
             {`"Thaddy's journey"`}
           </p>
-          <p className="text-black font-Inter font-normal text-[15px] capitalize">
+          <p className="text-black font-Inter font-normal text-[15px] ">
             Thaddeus has completed 6 cycles of chemotherapy, tumour removal
             surgery and is undergoing a chemotherapy + immunotherapy phase which
             consists of 5 cycles.
@@ -148,7 +198,7 @@ const Home: NextPage = () => {
           <br />
           <br />
 
-          <p className="text-black font-Inter font-normal text-[15px] capitalize">
+          <p className="text-black font-Inter font-normal text-[15px] ">
             Following this, the treatment will include stem cell transplant with
             high dose chemotherapy, radiation, immunotherapy and maintenance
             therapy consisting of a series of injections (vaccine) done
@@ -156,15 +206,15 @@ const Home: NextPage = () => {
           </p>
           <br />
 
-          <p className="text-black font-Inter font-normal text-[15px] capitalize">
-            Thaddeus has completed XX hospital stays and expects XX more in this
-            year. The hospital stays range from 4 days to 14 days. In between
-            hospital stays, Thaddeus will return to the hospital weekly for
-            scans, tests, blood tests and blood transfusions (if necessary).
-            Otherwise, he will have to rest at home and can only leave the house
-            when his blood test results are good. Even then, he must avoid
-            crowded places and dining out is not an option as he has to observe
-            a low bacteria diet.
+          <p className="text-black font-Inter font-normal text-[15px]">
+            Thaddeus has completed 13 hospital stays since Oct 21 and expects
+            more in this year. The hospital stays range from 4 days to 14 days.
+            In between hospital stays, Thaddeus will return to the hospital
+            weekly for scans, tests, blood tests and blood transfusions (if
+            necessary). Otherwise, he will have to rest at home and can only
+            leave the house when his blood test results are good. Even then, he
+            must avoid crowded places and dining out is not an option as he has
+            to observe a low bacteria diet.
           </p>
         </div>
       </div>
@@ -172,28 +222,48 @@ const Home: NextPage = () => {
         <p className="text-black font-Inter font-bold xs:text-[25px] sm:text-[25px] md:text-[40px] self-start mb-[20px] capitalize">
           {`"How can you contribute to this cause?"`}
         </p>
-        <p className="text-black font-Inter font-normal text-[15px] capitalize w-4/5">
+        <p className="text-black font-Inter font-normal text-[15px]  w-4/5">
           You can contribute by buying nfts in our collection or you can send
           donations in Give.Asia or gogetfunding.com
         </p>
       </div>
-      <div className=" px-[40px] my-[50px]">
+      <div className=" px-[40px] my-[50px] updates">
         <p className="text-black font-Inter font-bold xs:text-[25px] sm:text-[25px] md:text-[40px] self-start mb-[20px] capitalize">
           {`Updates`}
         </p>
-        <ol type="A">
-          <li className="text-black font-Inter font-bold xs:text-[15px] sm:text-[15px] md:text-[25px] self-start mb-[20px] capitalize">
-            Treatment
-          </li>
-          <li className="text-black font-Inter font-bold xs:text-[15px] sm:text-[15px] md:text-[25px] self-start mb-[20px] capitalize">
-            Funds Raised
-          </li>
-          <li className="text-black font-Inter font-bold xs:text-[15px] sm:text-[15px] md:text-[25px] self-start mb-[20px] capitalize">
-            Expenditure
-          </li>
-        </ol>
+        <div>
+          <div className="text-black font-Inter font-bold xs:text-[15px] sm:text-[15px] md:text-[20px] self-start mb-[20px] capitalize">
+            A)Treatment
+          </div>
+          <div className="flex flex-col">
+            <p className="text-black font-Inter font-bold xs:text-[15px] sm:text-[15px] md:text-[20px] self-start mb-[10px] capitalize">
+              B)Funds Raised
+            </p>
+            <div className="flex flex-col px-[10px] py-[20px] rounded-[10px]  w-fit shadow-lg">
+              <p className="text-black font-Inter font-bold xs:text-[10px] sm:text-[10px] md:text-[15px] self-start mb-[10px] capitalize">
+                Thomas
+              </p>
+              <p className="text-black font-Inter font-normal xs:text-[10px] sm:text-[10px] md:text-[15px] self-start  capitalize">
+                Donated $100
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-black font-Inter font-bold xs:text-[15px] sm:text-[15px] md:text-[20px] self-start  capitalize">
+              C)Expenditure
+            </p>
+            <div className="flex flex-row items-center">
+              <p className="text-black font-Inter font-normal xs:text-[10px] sm:text-[10px] md:text-[15px] ">
+                You can check the Expenditure sheet here:
+              </p>
+              <a href="https://drive.google.com/drive/folders/1C26ElFmluUNtKr-4zTAiLL5B__EkYqSP">
+                <DiGoogleDrive color="black" size={35} className="mr-[20px] " />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="w-screen h-auto flex flex-col bg-[#c49a11ba] pt-[50px] pb-[100px]  px-[40px]">
+      <div className="w-screen h-auto flex flex-col bg-[#e7c21d] pt-[50px] pb-[100px]  px-[40px] contactus">
         <p className="font-Inter text-white font-bold mb-[20px] text-[40px]">
           Contact Us
         </p>
